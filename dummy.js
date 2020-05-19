9f584dd35071f53738bb88d7b4af1827ba1e5109
JEFFGULICK <jeffgulick@gmail.com>
2020-05-18 20:38:42 -0500

    initial commit

---
+++ dummy.js
@@ -0,0 +1,13 @@
+const fetchTodos = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then( (response) => response.json())
+    .then( (json) => arrayOfTodos = json)
+  }
+
+  const logTodos = () => {
+    console.log(arrayOfTodos)
+  }
+
+  const populateTodos = () => {
+
+  }
