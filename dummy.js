a01956137e963a64c4fa6d0c37addec8b9baf3f3
JEFFGULICK <jeffgulick@gmail.com>
2020-05-19 00:26:58 -0500

    fixed first func

--- dummy.js
+++ dummy.js
@@ -12,7 +12,11 @@ const fetchTodos = () => {
   const populateTodos = () => {
     // console.log(arrayOfTodos[0].title)
     const element = document.getElementById("todo-list")
-    let listItem =  document.createElement("LI").innerHTML= arrayOfTodos[0].title
-    console.log(listItem)
 
+    let listItem =  document.createElement("LI")
+
+    listItem.innerHTML = arrayOfTodos[0].title
+    element.appendChild(listItem).innerHTML
+    console.log(element)
+    console.log(listItem)
   }
