3992a746ab91aa133b0d5a72e1740911c939337e
JEFFGULICK <jeffgulick@gmail.com>
2020-05-20 23:58:13 -0500

    finally got the append to work properly

--- dummy.js
+++ dummy.js
@@ -14,37 +14,34 @@ const fetchTodos = () => {
     //gets the element
     const element = document.getElementById("todo-list")
 
-    let doTitle = '';
     let doComplete ='';
 
     for(i=0; i<=199;i++){
-      doTitle = document.createTextNode(arrayOfTodos[i].title)
-      console.log(doTitle)
-
-      let item =  document.createElement("LI")//li element
+    
+      let item =  document.createElement("LI");//li element
 
       if(arrayOfTodos[i].completed == true){
-        item.innerHTML = arrayOfTodos[i].title.fontcolor("blue")
+        item.innerHTML = arrayOfTodos[i].title.fontcolor("blue");
+
       } else{
-        item.innerHTML = arrayOfTodos[i].title.fontcolor('red')
+        item.innerHTML = arrayOfTodos[i].title.fontcolor('red');
       }
 
+      item.append(' ');
+      item.append(arrayOfTodos[i].userId.toString());
+      item.append(' ');
+      item.append(arrayOfTodos[i].id.toString());
+      item.append(' ');
+      item.append(arrayOfTodos[i].completed);
+
+      doComplete = item;
+      console.log(doComplete);
       element.appendChild(item)
+
+
 
       // element.appendChild(item).html
 
     }
-
-    myArr = []
-    for(i=0; i<=199;i++){
-      a = arrayOfTodos[i].id
-      b = arrayOfTodos[i].userId
-      c = arrayOfTodos[i].title    
-      d = arrayOfTodos[i].completed
-      myArr.push(a, b, c, d)
-
-      console.log(myArr[0])
-    }
-
 
   }
