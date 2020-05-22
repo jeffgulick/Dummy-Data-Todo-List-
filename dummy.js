02dd694a7ef60e61c8a2296132f74e56aad6338d
JEFFGULICK <jeffgulick@gmail.com>
2020-05-21 20:54:46 -0500

    got everything working. added some jQuery. added basic styles.

--- dummy.js
+++ dummy.js
@@ -1,4 +1,4 @@
-
+//fetch and save as json format
 const fetchTodos = () => {
     fetch('https://jsonplaceholder.typicode.com/todos')
     .then( (response) => response.json())
@@ -11,32 +11,34 @@ const fetchTodos = () => {
 
   //func that populates fecth titles and inserts them in html list item
   const populateTodos = (place) => {
-    //gets the element
+    //gets the ol element
     const element = document.getElementById("todo-list")
 
     let doComplete ='';
 
     for(i=0; i<=199;i++){
     
-      let item =  document.createElement("LI");//li element
-
+      let item =  document.createElement("LI");// creates li element and assigns to var item//
+      //checking for status of completed//
       if(arrayOfTodos[i].completed == true){
-        item.innerHTML = arrayOfTodos[i].title.fontcolor("blue");
+        item.innerHTML = arrayOfTodos[i].title;//using the item because its the easiest to work with, is a string//
+        $(item).css('color', 'blue');//assigns all items that meat condition to the color blue with jQuery//
 
       } else{
-        item.innerHTML = arrayOfTodos[i].title.fontcolor('red');
+        item.innerHTML = arrayOfTodos[i].title;
+        $(item).css('color', 'red');
       }
-
+      //appending other object items to the var item
       item.append(' ');
-      item.append(arrayOfTodos[i].userId.toString());
+      item.append(arrayOfTodos[i].userId);
       item.append(' ');
-      item.append(arrayOfTodos[i].id.toString());
+      item.append(arrayOfTodos[i].id);
       item.append(' ');
       item.append(arrayOfTodos[i].completed);
 
-      doComplete = item;
-      console.log(doComplete);
-      element.appendChild(item)
+      // doComplete = item;
+      // console.log(doComplete);
+      element.appendChild(item);//appending what ive done to the html ol element above
 
 
 
